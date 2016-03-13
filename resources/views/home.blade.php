39739585a9a2f06193f7aba47dcb8898598599b4
David Lemayian <david@on.co.ke>
2016-03-13 14:23:46 +0300

    Basic home page look

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-4">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Forms</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <a href="form/create" class="btn btn-primary btn-wide"><i class="fa fa-plus"></i> New Form</a>
                 </div>
-            </div>
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </div>
+            </div> <!-- /.panel -->
+        </div> <!-- /.col-md-4 -->
+        <div class="col-md-8">
+
         </div>
     </div>
 </div>
