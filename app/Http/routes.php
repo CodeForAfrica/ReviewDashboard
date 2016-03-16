41bf48c146dc9ecf880c5f08301db952df61e675
David Lemayian <david@on.co.ke>
2016-03-16 15:14:42 +0300

    Add profile + Google oauth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,6 +37,8 @@ Route::group(['middleware' => 'web'], function () {
 
     Route::resource('form', 'FormController');
 
-    Route::get('/auth/social/redirect/{provider}',   ['as' => 'social.redirect',   'uses' => 'Auth\AuthController@getSocialRedirect']);
-    Route::get('/auth/social/handle/{provider}',     ['as' => 'social.handle',     'uses' => 'Auth\AuthController@getSocialHandle']);
+    Route::get('/profile', 'ProfileController@showProfile');
+
+    Route::get('/auth/google/redirect',   ['as' => 'social.redirect',   'uses' => 'Auth\AuthController@redirectToProvider']);
+    Route::get('/auth/google/handle',     ['as' => 'social.handle',     'uses' => 'Auth\AuthController@handleProviderCallback']);
 });
