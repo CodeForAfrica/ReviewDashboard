16edb1b70937c9f3e8032356bb3c7c19ea408fa0
David Lemayian <david@on.co.ke>
2016-03-16 16:06:07 +0300

    Added User Form relationship

--- app/User.php
+++ app/User.php
@@ -23,4 +23,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the forms for this user.
+     */
+    public function forms()
+    {
+        return $this->belongsToMany('App\Form');
+    }
 }
