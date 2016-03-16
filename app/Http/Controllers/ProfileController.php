fe9baa1b0d1681bda207dcbbf1dd81cc6b4da215
David Lemayian <david@on.co.ke>
2016-03-16 15:15:21 +0300

    We have a profile controller now.

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function getProfile()
+    {
+        return $this->showProfile();
+    }
+
+    public function showProfile(Request $request)
+    {
+        $data = array('user' => $request->user());
+        return view('profile', $data);
+    }
+
+    /**
+     * Update the user's profile.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function updateProfile(Request $request)
+    {
+        if ($request->user()) {
+            // $request->user() returns an instance of the authenticated user...
+        }
+    }
+}
