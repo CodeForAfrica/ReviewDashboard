e764dfeddd0123db0daf2c37fdf2d2863cf98e7e
David Lemayian <david@on.co.ke>
2016-07-01 08:33:48 +0300

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
+        <div class="col-sm-6 col-sm-offset-3">
+            <h3 class="page-head">Welcome</h3>
+            <p>The Review Dashboard helps you easily review applications.</p>
+            <p><a href="/login">Login</a> . <a href="/register">Register</a></p>
         </div>
     </div>
 </div>
